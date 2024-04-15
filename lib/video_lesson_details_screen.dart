d2145f95532065650196884bd597a44a03afa13c
NahomAndTebibu <funstore62@gmail.com>
2024-04-15 21:49:29 +0300

    video working

--- lib/video_lesson_details_screen.dart
+++ lib/video_lesson_details_screen.dart
@@ -1,16 +1,47 @@
+import 'dart:convert'; // Import dart:convert for json.decode
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:temari_bet_elearning_app/video_player_screen.dart';
 
-class VideoLessonDetailsScreen extends StatelessWidget {
+class VideoLessonDetailsScreen extends StatefulWidget {
   final String title;
 
   VideoLessonDetailsScreen({required this.title});
 
   @override
+  _VideoLessonDetailsScreenState createState() =>
+      _VideoLessonDetailsScreenState();
+}
+
+class _VideoLessonDetailsScreenState extends State<VideoLessonDetailsScreen> {
+  String _videoUrl = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchVideoUrl();
+  }
+
+  Future<void> _fetchVideoUrl() async {
+    try {
+      final response = await http.get(
+        Uri.parse('http://192.168.137.117:3000/getvideo?Title=${widget.title}'),
+      );
+      final responseData = json.decode(response.body);
+      setState(() {
+        _videoUrl = responseData['videoLink'];
+      });
+    } catch (error) {
+      print('Failed to fetch video URL: $error');
+      print(widget.title);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Video Lesson Details - $title'),
+        title: Text('Video Lesson Details - ${widget.title}'),
       ),
       body: SingleChildScrollView(
         child: Padding(
@@ -18,20 +49,21 @@ class VideoLessonDetailsScreen extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => VideoPlayerScreen(
-                        videoTitle: 'Sample Video',
-                        videoUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+              if (_videoUrl.isNotEmpty) // Remove null check
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => VideoPlayerScreen(
+                          videoTitle: widget.title,
+                          videoUrl: _videoUrl,
+                        ),
                       ),
-                    ),
-                  );
-                },
-                child: Text('Play Sample Video'),
-              ),
+                    );
+                  },
+                  child: Text('Play Video'),
+                ),
             ],
           ),
         ),
